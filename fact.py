fdeb596e9ee586e4245eb4b897be6ef512363c4c
keita shinohara <keita.shinohara@gmail.com>
2019-04-09 21:16:02 +0900

    add math.factorial() method

--- fact.py
+++ fact.py
@@ -1,3 +1,4 @@
+# lru_cache を使った自作関数
 from functools import lru_cache
 # import sys
 # sys.setrecursionlimit(500000)
@@ -8,7 +9,13 @@ def fact(number):
     if number == 0:
         return 1
     return number * fact(number - 1)
-
-
-n = 10
+n = 20
 print("Factorial is ", fact(n))
+
+
+# 標準モジュールを利用した場合
+import math
+
+
+fact_num = math.factorial(n)
+print("Factorial is ", fact_num)

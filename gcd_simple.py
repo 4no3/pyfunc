154a41a1a5554ecb1d37ae9123969833c221389f
keita shinohara <keita.shinohara@gmail.com>
2019-05-18 16:22:52 +0900

    Add my gcd function. Add lcm process for multiple numbers

--- gcd_simple.py
+++ gcd_simple.py
@@ -2,6 +2,14 @@ from math import gcd        # Python3.5 以降
 # from fractions import gcd   # Python3.4 以前
 
 
-a = 429
-b = 286
-print("GCD is ", gcd(a, b))
+# my function
+# def gcd(a, b):
+#     if b == 0:
+#         return a
+#     return gcd(b, a%b)
+
+
+# 動作確認
+# a = 429
+# b = 286
+# print("GCD is ", gcd(a, b))
--- lcm_simple.py
+++ lcm_simple.py
@@ -5,6 +5,14 @@ def lcm_simple(num1, num2):
     return (num1 * num2) // gcd(num1, num2)
 
 
+# 動作確認
 a = 6
 b = 9
 print(lcm_simple(a, b))
+
+
+lst = [6, 21, 12, 33]
+lcm = 1
+for a in lst:
+    lcm = lcm_simple(lcm, a)
+print(lcm)
